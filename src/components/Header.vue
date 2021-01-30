7216285b1f6b032881da771a86f6f9ba9901633c
Rob Ranf <robranf@gmail.com>
2021-01-30 13:35:40 -0800

    split sections into components

---
+++ src/components/Header.vue
@@ -0,0 +1,187 @@
+<template>
+  <div id="header">
+    <header class="relative">
+      <div class="bg-white pt-6 mb-2">
+        <nav
+          class="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
+          aria-label="Global"
+        >
+          <div class="flex items-center flex-1">
+            <div class="flex items-center justify-between w-full md:w-auto">
+              <a href="#">
+                <span class="sr-only">Emiya Consulting Home Page</span>
+                <img
+                  class="h-8 w-auto sm:h-10"
+                  src="../assets/ec_new_logo_tags_small.svg"
+                  alt=""
+                />
+              </a>
+              <div class="-mr-2 flex items-center md:hidden">
+                <button
+                  type="button"
+                  class="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white"
+                  id="main-menu"
+                  aria-haspopup="true"
+                >
+                  <span class="sr-only">Open main menu</span>
+                  <!-- Heroicon name: menu -->
+                  <svg
+                    class="h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+            <div class="hidden space-x-8 md:flex md:ml-10">
+              <a
+                href="#"
+                class="text-base font-medium text-black hover:text-gray-300"
+                >Services</a
+              >
+
+              <a
+                href="#"
+                class="text-base font-medium text-black hover:text-gray-300"
+                >C.V.</a
+              >
+
+              <a
+                href="#"
+                class="text-base font-medium text-black hover:text-gray-300"
+                >Portfolio</a
+              >
+            </div>
+          </div>
+          <div class="hidden md:flex md:items-center md:space-x-6">
+            <!-- <a
+              href="#"
+              class="text-base font-medium text-black hover:text-gray-300"
+            >
+              Log In
+            </a> -->
+            <a
+              href="#"
+              class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700"
+            >
+              Contact Us
+            </a>
+          </div>
+        </nav>
+      </div>
+
+      <!--
+        Mobile menu, show/hide based on menu open state.
+
+        Entering: "duration-150 ease-out"
+          From: "opacity-0 scale-95"
+          To: "opacity-100 scale-100"
+        Leaving: "duration-100 ease-in"
+          From: "opacity-100 scale-100"
+          To: "opacity-0 scale-95"
+      -->
+      <div
+        class="absolute top-0 inset-x-0 p-2 transition transform origin-top md:hidden"
+      >
+        <div
+          class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden"
+        >
+          <div class="px-5 pt-4 flex items-center justify-between">
+            <div>
+              <img
+                class="h-8 w-auto sm:h-10"
+                src="../assets/ec_new_logo_tags_small.svg"
+                alt=""
+              />
+            </div>
+            <div class="-mr-2">
+              <button
+                type="button"
+                class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600"
+              >
+                <span class="sr-only">Close menu</span>
+                <!-- Heroicon name: x -->
+                <svg
+                  class="h-6 w-6"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+          <div
+            class="pt-5 pb-6"
+            role="menu"
+            aria-orientation="vertical"
+            aria-labelledby="main-menu"
+          >
+            <div class="px-2 space-y-1" role="none">
+              <a
+                href="#"
+                class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
+                role="menuitem"
+                >Services</a
+              >
+
+              <a
+                href="#"
+                class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
+                role="menuitem"
+                >C.V.</a
+              >
+
+              <a
+                href="#"
+                class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
+                role="menuitem"
+                >Portfolio</a
+              >
+            </div>
+            <div role="none" class="mt-6 px-5">
+              <a
+                href="#"
+                class="block text-center w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700"
+                >Contact Us</a
+              >
+            </div>
+            <!-- <div role="none" class="mt-6 px-5">
+              <p class="text-center text-base font-medium text-gray-500">
+                Existing customer?
+                <a href="#" class="text-gray-900 hover:underline">Login</a>
+              </p>
+            </div> -->
+          </div>
+        </div>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,25 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <app-main></app-main>
+    <app-services></app-services>
+    <app-about></app-about>
+    <app-portfolio></app-portfolio>
+  </div>
 </template>
 
 <script>
+import Main from "@/components/Main.vue";
+import Services from "@/components/Services.vue";
+import About from "@/components/About.vue";
+import Portfolio from "@/components/Portfolio.vue";
+
 export default {
   name: "Home",
-  components: {},
+  components: {
+    appMain: Main,
+    appServices: Services,
+    appAbout: About,
+    appPortfolio: Portfolio,
+  },
 };
 </script>
