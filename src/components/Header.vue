712288d0f40cf2e41f6127586e8a94ad8d0dabe5
Rob Ranf <robranf@gmail.com>
2021-01-31 12:36:21 -0800

    fixing conditional rendering on mobile menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="header">
-    <header class="relative">
-      <div class="bg-white pt-6 mb-2">
+    <header class="relative mb-2">
+      <div class="bg-white pt-6">
         <nav
           class="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6"
           aria-label="Global"
@@ -9,19 +9,18 @@
           <div class="flex items-center flex-1">
             <div class="flex items-center justify-between w-full md:w-auto">
               <a href="#">
-                <span class="sr-only">Emiya Consulting Home Page</span>
+                <span class="sr-only">Workflow</span>
                 <img
                   class="h-8 w-auto sm:h-10"
                   src="../assets/ec_new_logo_tags_small.svg"
                   alt=""
                 />
               </a>
-
-              <!-- Open mobile menu button -->
-              <div class="-mr-2 flex items-center lg:hidden">
+              <div class="-mr-2 flex items-center md:hidden">
                 <button
                   type="button"
-                  class="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white"
+                  @click="openMenu"
+                  class="bg-white rounded-md p-2 inline-flex items-center justify-center text-emiyablack hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white"
                   id="main-menu"
                   aria-haspopup="true"
                 >
@@ -48,33 +47,33 @@
             <div class="hidden space-x-8 md:flex md:ml-10">
               <a
                 href="#"
-                class="text-base font-medium text-black hover:text-gray-300"
+                class="text-base font-medium text-emiyablack hover:text-gray-300"
                 >Services</a
               >
 
               <a
                 href="#"
-                class="text-base font-medium text-black hover:text-gray-300"
-                >Portfolio</a
+                class="text-base font-medium text-emiyablack hover:text-gray-300"
+                >About</a
               >
 
               <a
                 href="#"
-                class="text-base font-medium text-black hover:text-gray-300"
-                >About</a
+                class="text-base font-medium text-emiyablack hover:text-gray-300"
+                >Portfolio</a
               >
             </div>
           </div>
           <div class="hidden md:flex md:items-center md:space-x-6">
             <!-- <a
               href="#"
-              class="text-base font-medium text-black hover:text-gray-300"
+              class="text-base font-medium text-emiyablack hover:text-gray-300"
             >
               Log In
             </a> -->
             <a
               href="#"
-              class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gradient-to-r from-gray-600 to-emiyablue hover:from-gray-500 hover:to-emiyablue80"
+              class="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700"
             >
               Contact Us
             </a>
@@ -82,18 +81,19 @@
         </nav>
       </div>
 
-      <!--
-        Mobile menu, show/hide based on menu open state.
-
-        Entering: "duration-150 ease-out"
-          From: "opacity-0 scale-95"
-          To: "opacity-100 scale-100"
-        Leaving: "duration-100 ease-in"
-          From: "opacity-100 scale-100"
-          To: "opacity-0 scale-95"
-      -->
+      <!-- Mobile menu, show/hide based on menu open state. -->
+      <transition
+        enter-active-class="duration-150 ease-out"
+        enter-from-class="opacity-0 scale-95"
+        enter-to-class="opacity-100 scale-100"
+        leave-active-class="duration-100 ease-in"
+        leave-from-class="opacity-100 scale-100"
+        leave-to-class="opacity-0 scale-95"
+      >
+      </transition>
       <div
-        class="absolute top-0 inset-x-0 p-2 transition transform origin-top lg:hidden"
+        v-show="isOpen"
+        class="absolute top-0 inset-x-0 p-2 transition transform origin-top md:hidden"
       >
         <div
           class="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden"
@@ -101,7 +101,7 @@
           <div class="px-5 pt-4 flex items-center justify-between">
             <div>
               <img
-                class="h-8 w-auto sm:h-10"
+                class="h-8 w-auto"
                 src="../assets/ec_new_logo_tags_small.svg"
                 alt=""
               />
@@ -109,7 +109,8 @@
             <div class="-mr-2">
               <button
                 type="button"
-                class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600"
+                @click="closeMenu"
+                class="bg-white rounded-md p-2 inline-flex items-center justify-center text-emiyablack hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600"
               >
                 <span class="sr-only">Close menu</span>
                 <!-- Heroicon name: x -->
@@ -140,38 +141,38 @@
             <div class="px-2 space-y-1" role="none">
               <a
                 href="#"
-                class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
+                class="block px-3 py-2 rounded-md text-base font-medium text-emiyablack hover:bg-gray-50"
                 role="menuitem"
                 >Services</a
               >
 
               <a
                 href="#"
-                class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
+                class="block px-3 py-2 rounded-md text-base font-medium text-emiyablack hover:bg-gray-50"
                 role="menuitem"
-                >Portfolio</a
+                >About</a
               >
 
               <a
                 href="#"
-                class="block px-3 py-2 rounded-md text-base font-medium text-gray-900 hover:bg-gray-50"
+                class="block px-3 py-2 rounded-md text-base font-medium text-emiyablack hover:bg-gray-50"
                 role="menuitem"
-                >About</a
+                >Portfolio</a
               >
             </div>
             <div role="none" class="mt-6 px-5">
               <a
                 href="#"
                 class="block text-center w-full py-3 px-4 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700"
-                >Contact Us</a
+                >Start free trial</a
               >
             </div>
-            <!-- <div role="none" class="mt-6 px-5">
+            <div role="none" class="mt-6 px-5">
               <p class="text-center text-base font-medium text-gray-500">
                 Existing customer?
                 <a href="#" class="text-gray-900 hover:underline">Login</a>
               </p>
-            </div> -->
+            </div>
           </div>
         </div>
       </div>
@@ -184,12 +185,15 @@ export default {
   name: "Header",
   data() {
     return {
-      isHidden: true,
+      isOpen: false,
     };
   },
   methods: {
-    hideMobileMenu() {
-      this.isHidden ? (this.isHidden = false) : (this.isHidden = true);
+    openMenu() {
+      this.isOpen = true;
+    },
+    closeMenu() {
+      this.isOpen = false;
     },
   },
 };
