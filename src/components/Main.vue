711c24f08e5871419ecea45e93bca2e8837445cb
Rob Ranf <rob@emiyaconsulting.com>
2021-07-16 10:22:48 -0700

    updating padding on main title and text to better center the whole main section

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -7,20 +7,54 @@
         <div class="mx-auto max-w-7xl lg:px-8">
           <div class="lg:grid lg:grid-cols-2 lg:gap-8">
             <div
-              class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 sm:text-center lg:px-0 lg:text-left lg:flex lg:items-center"
+              class="
+                mx-auto
+                max-w-md
+                px-4
+                sm:max-w-2xl
+                sm:px-6
+                sm:text-center
+                lg:px-0
+                lg:text-left
+                lg:flex
+                lg:items-center
+              "
             >
-              <div class="lg:py-20">
+              <div class="lg:pl-16 lg:py-20">
                 <h1
-                  class="mt-4 text-4xl tracking-tight font-extrabold sm:mt-5 sm:text-6xl lg:mt-6 xl:text-5xl"
+                  class="
+                    mt-4
+                    text-4xl
+                    tracking-tight
+                    font-extrabold
+                    sm:mt-5
+                    sm:text-6xl
+                    lg:mt-6
+                    xl:text-5xl
+                  "
                 >
                   <span class="text-white block">You mind your business</span>
                   <span
-                    class="bg-clip-text text-transparent bg-gradient-to-r from-white to-emiyablue block"
+                    class="
+                      bg-clip-text
+                      text-transparent
+                      bg-gradient-to-r
+                      from-white
+                      to-emiyablue
+                      block
+                    "
                     >We'll mind your tech</span
                   >
                 </h1>
                 <p
-                  class="mt-3 text-base text-white sm:mt-5 sm:text-xl lg:text-lg xl:text-xl"
+                  class="
+                    mt-3
+                    text-base text-white
+                    sm:mt-5
+                    sm:text-xl
+                    lg:text-lg
+                    xl:text-xl
+                  "
                 >
                   You're not in business to write code, manage tech
                   infrastructure or deal with tech vendors. Let us build and
@@ -31,11 +65,27 @@
             </div>
             <div class="mt-12 -mb-16 sm:-mb-48 lg:m-0 lg:relative">
               <div
-                class="mx-auto max-w-md px-4 sm:max-w-2xl sm:px-6 lg:max-w-none lg:px-0"
+                class="
+                  mx-auto
+                  max-w-md
+                  px-4
+                  sm:max-w-2xl
+                  sm:px-6
+                  lg:max-w-none
+                  lg:px-0
+                "
               >
                 <!-- Illustration taken from Lucid Illustrations: https://lucid.pixsellz.io/ -->
                 <img
-                  class="w-full lg:absolute lg:inset-y-0 lg:left-0 lg:h-full lg:w-full lg:max-w-none"
+                  class="
+                    w-full
+                    lg:absolute
+                    lg:inset-y-0
+                    lg:left-0
+                    lg:h-full
+                    lg:w-full
+                    lg:max-w-none
+                  "
                   src="../assets/illustration_main.svg"
                   alt="Main section illustration - Emiya Consulting, Web site design and development, mobile app development, and small business technology consulting in the Central Puget Sound area"
                 />
