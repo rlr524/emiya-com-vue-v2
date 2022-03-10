9dbf891ef40a6d520684d7c1e70c39039c1c650a
Rob Ranf <rob@emiyaconsulting.com>
2022-03-10 13:11:44 -0800

    adding mailto icon

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -56,6 +56,25 @@
                 />
               </svg>
             </a>
+
+            <a
+              href="mailto:rob+emiyacom@emiyaconsulting.com"
+              class="text-gray-400 hover:text-gray-500"
+            >
+              <span class="sr-only">Email Us</span>
+              <svg
+                class="h-6 w-6"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"
+                  clip-rule="evenodd"
+                />
+              </svg>
+            </a>
           </div>
         </div>
         <div class="flex justify-left mt-6 border-t border-gray-200 py-8">
